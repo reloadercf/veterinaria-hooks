0341f4a6ceb4ff863096aa7c681179954106a06b
reloadercf <elcfm1@gmail.com>
2019-10-04 18:43:44 -0500

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React,{useState,Fragment} from 'react'
+import React,{useState,Fragment,useEffect} from 'react'
+
+function Cita({cita,index,eliminarCita}){
+  return(
+    <div className='cita'>
+      <p>Mascota:<span>{cita.mascota}</span></p>
+      <p>Dueño: <span>{cita.propietario}</span></p>
+      <p>Fecha: <span>{cita.fecha}</span></p>
+      <p>Hora: <span>{cita.hora}</span></p>
+      <p>Sintomas: <span>{cita.sintomas}</span></p>
+      <button 
+        onClick={()=>eliminarCita(index)}
+        type='button' className='button eliminar u-full_width'>Eliminar X</button>
+    </div>
+  )
+
+}
 
 function Formulario({crearCita}){
-  let [cita, actualizarCita]=useState({
+  let stateReiniciarInicial={
     mascota:'',
     propietario:'',
     fecha:'',
     hora:'',
     sintomas:'',
-  });
+  }
 
+  //cita = state Reiniciar Inicial
+  let [cita, actualizarCita]=useState(stateReiniciarInicial);
 
+//actualiza el state
 let actualizarState=(e)=>{
 actualizarCita({
   ...cita,
@@ -17,12 +36,14 @@ actualizarCita({
 })
 }
 
+//pasamos la cita al componente principal
 let enviarCita=(e)=>{
   e.preventDefault();
   //lo siguiete es pasar la cita hacia el componente principal
   console.log(cita)
-  //despues reiniciar el state y el form
   crearCita(cita)
+  //despues reiniciar el state y el form
+  actualizarCita(stateReiniciarInicial)
 }
 
   return (
@@ -37,6 +58,7 @@ let enviarCita=(e)=>{
                     className="u-full-width" 
                     placeholder="Nombre Mascota" 
                     onChange={actualizarState}
+                    value={cita.mascota}
                   />
 
                   <label>Nombre Dueño</label>
@@ -46,6 +68,7 @@ let enviarCita=(e)=>{
                     className="u-full-width"  
                     placeholder="Nombre Dueño de la Mascota" 
                     onChange={actualizarState}
+                    value={cita.propietario}
                   />
 
                   <label>Fecha</label>
@@ -54,6 +77,7 @@ let enviarCita=(e)=>{
                     className="u-full-width"
                     name="fecha"
                     onChange={actualizarState}
+                    value={cita.fecha}
                   />               
 
                   <label>Hora</label>
@@ -62,6 +86,7 @@ let enviarCita=(e)=>{
                     className="u-full-width"
                     name="hora" 
                     onChange={actualizarState}
+                    value={cita.hora}
                   />
 
                   <label>Sintomas</label>
@@ -69,6 +94,7 @@ let enviarCita=(e)=>{
                     className="u-full-width"
                     name="sintomas"
                     onChange={actualizarState}
+                    value={cita.sintomas}
                   ></textarea>
 
                   <button type="submit" className="button-primary u-full-width">Agregar</button>
@@ -78,10 +104,15 @@ let enviarCita=(e)=>{
 }
 
 function App (){
+  //cargaremos las citas del localstorage como state inicial
+  let citasIniciales=JSON.parse(localStorage.getItem('citas'))
+  if(!citasIniciales){
+    citasIniciales=[]
+  }
 //use state returona 2 piesas de funciones
 
 //state es igual a tener el state actual
-let [citas, guardarCitas]=useState([]);
+let [citas, guardarCitas]=useState(citasIniciales);
 //el segundo es la que actualiza el state
 
 //necesitamos a continuacion un metodo para agregar las nuevas citas al state
@@ -92,6 +123,34 @@ let crearCita=cita=>{
   guardarCitas(nuevasCitas)
 }
 
+//Elimina las citas del state
+
+let eliminarCita=index=>{
+  let nuevasCitas=[...citas]
+  nuevasCitas.splice(index,1)
+  guardarCitas(nuevasCitas)
+}
+
+//ya no se usa component dis mout y tampoco component willmount
+useEffect(
+  ()=>{
+    // console.log('componente listo o algo cambio')
+    let citasIniciales=JSON.parse(localStorage.getItem('citas'))
+    //vamos a guardar todo en el local storage
+    //iniciamos con leer si hay sitas
+    if(citasIniciales){
+      localStorage.setItem('citas', JSON.stringify(citas))
+    }else {
+      //el else funciona para crear el local storage
+      localStorage.setItem('citas',JSON.stringify([]))
+    }
+  },[citas]
+)
+
+
+//cargar dinamicamente un titulo
+let titulo=Object.keys(citas).length===0?'No hay citas':'administrar las citas aqui'
+
 return(<Fragment>
   <h1>Administrador de pacientes</h1>
   <div className='container'>
@@ -100,7 +159,15 @@ return(<Fragment>
         <Formulario crearCita={crearCita} />
       </div>
       <div className='one-half column'>
-      
+        <h2>{titulo}</h2>
+      {citas.map((cita, index)=>(
+        <Cita
+          key={index}
+          index={index}
+          cita={cita}
+          eliminarCita={eliminarCita}
+        />
+      ))}
       </div>
     </div>
   </div>
